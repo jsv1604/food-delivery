4e9bc4c91476a2fc2a61a691dd032b62f2d29457
Jeffrey S Varghese <jeff0716042@gmail.com>
2023-09-08 17:22:52 +0530

    session 6 schema creation

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb+srv://admin:admin@cluster0.ddt3a2g.mongodb.net/?retryWrites=true&w=majority"
+const mongoURI = "mongodb+srv://admin:admin@cluster0.ddt3a2g.mongodb.net/food-delivery-mern?retryWrites=true&w=majority"
 const mongoDB = async() =>{
     await mongoose.connect(mongoURI, { useNewUrlParser : true})
-    .then(()=>{
-        console.log("connected");
+    .then(async()=>{
+        console.log("MongoDB connected successfully");
+        const fetched_data = await mongoose.connection.db.collection("foodCategory");
+        fetched_data.find({}).toArray()
+        .then((data) =>{
+            console.log("Data recieved");
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+        
     })
     .catch((err) =>{
         console.log(err);
