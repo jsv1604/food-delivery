8125e3cfe238c8429eea25e1df7caa0dea8a4516
Jeffrey S Varghese <jeff0716042@gmail.com>
2023-09-15 19:55:15 +0530

    login implemented

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -3,8 +3,10 @@ const router = express.Router()
 const User = require("../models/User")
 const { body, validationResult } = require('express-validator');
 
+
+
 router.post("/createuser",
-[body('email','Invalid Email ID').isEmail(),
+[body('email','Not a valid Email').isEmail(),
 body('name','Name should have more than 5 characters').isLength({min:5}),
 body('password','Password should have more than 5 characters').isLength({min:5})],
  async(req,res)=>{
@@ -22,6 +24,39 @@ body('password','Password should have more than 5 characters').isLength({min:5})
             location:req.body.location,
             
         })
+        console.log("User created");
+        res.json({success:true});
+    }catch (error){
+        console.log(error)
+        res.json({success:false});
+    }
+})
+
+
+
+
+router.post("/loginuser",
+[body('email','Not a valid Email').isEmail()],
+ async(req,res)=>{
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({error : errors.array()});
+    }
+
+    try{
+        let email = req.body.email
+        let userData = await User.findOne({email});
+        if(!userData){
+            return res.status(400).json({error: "Email not registered, Login with Valid credentials"})
+        }
+        
+        if(! (req.body.password === userData.password)){
+            
+            return res.status(400).json({error: "Wrong password, Enter correct password"})
+        }
+
+        return res.json({success: true})
         console.log("User created");
         res.json({success:true});
     }catch (error){
